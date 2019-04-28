0d2375e568f66ac279e13a82df7934aa461a0814
Jag_k <admin@jag-k.info>
2019-04-28 17:55:18 +0300

    Added edit and delete blog post

--- __main__.py
+++ __main__.py
@@ -1,7 +1,20 @@
-import math
-
 from lib import *
 from markdown import markdown
+
+
+def blog_short(t):
+    r = ""
+    write = True
+    for letter in t:
+        if letter == "<":
+            write = False
+            continue
+        elif letter == ">":
+            write = True
+            continue
+        if write:
+            r += letter
+    return r
 
 
 # #   ADMIN ROUTING   # #
@@ -13,15 +26,37 @@ def admin_page():
     )
 
 
-@admin_route('/blog/new', ["GET", "POST"])
-def new_post():
-    if request.method == "POST":
+@admin_route("/blog")
+def admin_blog():
+    return admin_temp("blog", "Блог", posts=posts)
+
+
+@admin_route('/blog/delete')
+def delete_post():
+    post_id = request.query.id
+    post_ids = list(map(lambda x: x["title"], posts))
+    if not post_id or post_id not in post_ids:
+        alert = Alert("Такого поста не существует", Alert.DANGER)
+        return redirect('/admin/blog', alert=alert)
+
+    del posts[post_ids.index(post_id)]
+    dump(posts, open(POSTS_FILE, 'w'))
+    return redirect('/admin/blog', alert=Alert("Пост успешно удалён"))
+
+
+@admin_route("/blog/edit", method=[GET, POST])
+def edit_post():
+    post_id = request.query.id
+    post_ids = list(map(lambda x: x["title"], posts))
+
+    if not post_id or post_id not in post_ids:
+        alert = Alert("Такого поста не существует", Alert.DANGER)
+        return redirect('/admin/blog', alert=alert)
+
+    post = posts[post_ids.index(post_id)]
+
+    if request.method == POST:
         params = dict(request.params)
-        if params['title'] in map(lambda x: x["title"], posts):
-            return redirect('/admin/blog/new', alert={
-                "content": "Пост в блоге с таким заголовком (%s) уже существует! Попробуйте другой" % params['title'],
-                "type": "danger"
-            })
         res = {"title": params['title']}
         files = dict(request.files)  # type: Dict[str, bottle.FileUpload]
 
@@ -30,23 +65,59 @@ def new_post():
 
         else:
             file = files['image']
-            file.name: str
-            print(file.filename)
             filename = params['title'] + '.' + file.filename.rsplit('.', 1)[-1]
             file.save('./public/images/' + filename, overwrite=True)
 
             res['img'] = filename
 
+        res['raw_content'] = params['blog_post']
         res['content'] = markdown(params['blog_post'])
-        res['short'] = params['blog_post'].split('.')[0]
+
+        res['short'] = blog_short(res['content'])
+        res['short'] = res['short'][:87] + '...' if len(res['short']) >= 90 else res['short']
+
+        posts[post_ids.index(post_id)] = res
+        dump(posts, open(POSTS_FILE, 'w'), ensure_ascii=False, indent=2)
+        return redirect('/admin/blog', alert=Alert('Пост "%s" успешно отредактирован!' % post['title']))
+
+    return admin_temp('new_blog', "Редактирование поста", post=post)
+
+
+@admin_route('/blog/new', [GET, POST])
+def new_post():
+    if request.method == POST:
+        params = dict(request.params)
+        if params['title'] in map(lambda x: x["title"], posts):
+
+            return redirect('/admin/blog/new', alert=Alert(
+                "Пост в блоге с таким заголовком (%s) уже существует! Попробуйте другой" % params['title'],
+                Alert.DANGER
+            ))
+        res = {"title": params['title']}
+        files = dict(request.files)  # type: Dict[str, bottle.FileUpload]
+
+        if not files:
+            res['img'] = None
+
+        else:
+            file = files['image']
+            filename = params['title'] + '.' + file.filename.rsplit('.', 1)[-1]
+            file.save('./public/images/' + filename, overwrite=True)
+
+            res['img'] = filename
+
+        res['raw_content'] = params['blog_post']
+        res['content'] = markdown(params['blog_post'])
+
+        res['short'] = blog_short(res['content'])
+        res['short'] = res['short'][:87] + '...' if len(res['short']) >= 90 else res['short']
 
         posts.append(res)
-        dump(posts, open(POSTS_FILE, 'w'), indent=2)
+        dump(posts, open(POSTS_FILE, 'w'), ensure_ascii=False, indent=2)
 
-        return redirect('/admin', alert={
-            "content": "Пост успешно создан!",
-            "type": "success"
-        })
+        return redirect('/admin/blog', alert=Alert(
+            "Пост успешно создан!"
+        ))
     return admin_temp('new_blog', "Новый пост в блоге")
 
 
@@ -58,13 +129,13 @@ BLOG_POSTS = 7
 def main_page():
     return template(
         "main",
-        posts=posts,
+        posts=list(reversed(posts))[:2],
     )
 
 
 @route("/blog")
 def blog():
-    cur_page = request.params.get("page")
+    cur_page = request.query.getunicode("from")
     max_pages = len(posts) // BLOG_POSTS + bool(len(posts) % BLOG_POSTS)
     if cur_page is not None and (not (cur_page.isdigit() and int(cur_page) > 0)) or cur_page == "1":
         return redirect('/blog')
@@ -88,19 +159,22 @@ def blog_post(post=""):
     return template('blog_post', p[0]['title'] + " (Блог)", **p[0])
 
 
-@route("/login", method=["GET", "POST"])
+@route(ADMIN_LOGIN_ROUTE, method=["GET", "POST"])
 def login():
-    alert = {}
+    alert = None
     if request.method == "POST":
         h = hash_admin(**request.params)
         if is_hash_admin(h):
             response.set_cookie(ADMIN_COOKIE_KEY, h, ADMIN_COOKIE_SECRET, max_age=604800, httponly=True)
-            return redirect("/admin")
+            redirect_from = request.get_cookie("redirect", "/admin", ADMIN_COOKIE_SECRET)
+            return redirect(redirect_from)
         else:
-            alert = {
-                'content': "Вы ввели не правильный логин или пароль! Повторите снова",
-                'type': "danger"
-            }
+            alert = Alert(
+                "Вы ввели не правильный логин или пароль! Повторите снова",
+                Alert.DANGER
+            )
+    if request.params.get("from"):
+        response.set_cookie('redirect', request.params.get("from", '/admin'), ADMIN_COOKIE_SECRET)
     return template("login",
                     template_title="Вход в админку",
                     alert=alert,
--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
 # /usr/bin/python3
-from functools import wraps, partial
+from functools import wraps
 from os.path import join
 from hashlib import pbkdf2_hmac
 from sys import stderr
@@ -15,24 +15,43 @@ except ImportError:
     print("Please, install ujson module (pip3 install ujson)", file=stderr)
     from json import load, dump, loads, dumps
 
-
+GET, POST, DELETE, PATCH = "GET", "POST", "DELETE", "PATCH"
 app = bottle.Bottle()
 bottle.ERROR_PAGE_TEMPLATE = open("view/error.html").read()
 
 
-@wraps(bottle.Bottle.route)
-def route(p=None, method='GET', callback=None, name=None,
-          apply=None, skip=None, **config):
+class Alert:
+    PRIMARY = "primary"
+    SECONDARY = "secondary"
+    SUCCESS = "success"
+    DANGER = "danger"
+    WARNING = "warning"
+    INFO = "info"
+    LIGHT = "light"
+    DARK = "dark"
 
-    def _(func):
-        app.route(p, method, callback, name,
-                  apply, skip, **config)(func)
+    def __init__(self, content: str, alert_type=SUCCESS):
+        self.content = content
+        self.alert_type = alert_type
 
-        path = (p + '/').replace('//', '/')
-        app.route(path, method, callback, name,
-                  apply, skip, **config)(func)
-        return func
-    return _
+    @property
+    def conv(self):
+        return {
+            "content": self.content,
+            "type": self.alert_type
+        }
+
+    def __dir__(self):
+        return {
+            "content": self.content,
+            "type": self.alert_type
+        }
+
+    def __str__(self):
+        return 'Alert(%s): "%s"' % (self.alert_type.capitalize(), self.content)
+
+    def __repr__(self):
+        return "<Alert(%s) %.10s>" % (self.alert_type.capitalize(), self.content)
 
 
 # #   ADMINS   # #
@@ -70,7 +89,7 @@ def admin_route(url, method="GET"):
             user = request.get_cookie(ADMIN_COOKIE_KEY, None, ADMIN_COOKIE_SECRET)
             if is_hash_admin(user):
                 return func(*args, **kwargs)
-            bottle.redirect(ADMIN_LOGIN_ROUTE)
+            bottle.redirect(ADMIN_LOGIN_ROUTE + "?from=/admin" + url.rstrip("/"))
         return wrapped
     return _
 
@@ -85,47 +104,40 @@ posts = load(open(POSTS_FILE))  # type: List[Dict[str, str]]
 
 
 # #   MAIN   # #
-def template(source, template_title="", extension=".html", skeleton="view/skeleton.html", including_page=None, *args,
-             **kwargs):
+def template(source, template_title="", extension=".html", skeleton="view/skeleton.html", including_page=None,
+             alert: Alert = None, *args, **kwargs):
     d = loads(request.get_cookie("kwargs", "{}", ADMIN_COOKIE_SECRET))
     if d:
-        response.delete_cookie("kwargs")
+        response.delete_cookie("kwargs", path='/')
+        if 'alert' in d:
+            alert = loads(d['alert'])
         kwargs.update(d)
     return bottle.template(skeleton, title=template_title,
                            is_admin=is_hash_admin(request.get_cookie(ADMIN_COOKIE_KEY, None, ADMIN_COOKIE_SECRET)),
                            including_page=including_page or join("view", source + extension),
-                           args=args, kwargs=kwargs)
+                           args=args, alert=alert, kwargs=kwargs)
 
 
 @wraps(bottle.redirect)
-def redirect(url, code=None, **kwargs):
-    if kwargs:
-        response.set_cookie("kwargs", dumps(kwargs), ADMIN_COOKIE_SECRET)
+def redirect(url, code=None, alert: Alert = None, **kwargs):
+    if kwargs or alert:
+        if alert:
+            kwargs['alert'] = dumps(alert.conv)
+        response.set_cookie("kwargs", dumps(kwargs), ADMIN_COOKIE_SECRET, path='/')
     return bottle.redirect(url, code)
 
 
-class Alert:
-    PRIMARY = "primary"
-    SECONDARY = "secondary"
-    SUCCESS = "success"
-    DANGER = "danger"
-    WARNING = "warning"
-    INFO = "info"
-    LIGHT = "light"
-    DARK = "dark"
+@wraps(bottle.Bottle.route)
+def route(p=None, method='GET', callback=None, name=None,
+          apply=None, skip=None, **config):
 
-    def __init__(self, content: str, alert_type=SUCCESS):
-        self.content = content
-        self.alert_type = alert_type
+    def _(func):
+        app.route(p, method, callback, name,
+                  apply, skip, **config)(func)
 
-    def __dir__(self):
-        return {
-            "content": self.content,
-            "type": self.alert_type
-        }
+        path = (p + '/').replace('//', '/')
+        app.route(path, method, callback, name,
+                  apply, skip, **config)(func)
+        return func
+    return _
 
-    def __str__(self):
-        return 'Alert(%s): "%s"' % (self.alert_type.capitalize(), self.content)
-
-    def __repr__(self):
-        return "<Alert(%s) %.10s>" % (self.alert_type.capitalize(), self.content)
